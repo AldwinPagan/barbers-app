cd0ffa8cf0a4a094dcf94403e18249646b5243a8
AldwinPagan <aldwin.pagan@gmail.com>
2021-07-29 21:39:15 -0400

    [UPDATE] from react-router-dom to @reach/router

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,17 +1,24 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router } from "@reach/router";
 import App from "./App";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import TenantPage from "./pages/TenantPage";
 import { FC } from "react";
-
+import { Booking } from "./modules/events/components";
 export const AppRoutes: FC = () => {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={App} />
-      <Route path="/auth/login" component={LoginPage} />
-      <Route path="/auth/forgot-password" component={ForgotPasswordPage} />
-      <Route exact path="/tenant/:tenantId" component={TenantPage} />
-    </BrowserRouter>
+    <Router>
+      {/* <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/auth/login" component={LoginPage} />
+        <Route path="/auth/forgot-password" component={ForgotPasswordPage} />
+        <Route path="/tenant/:tenantId" component={TenantPage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch> */}
+      <App path="/" />
+      <TenantPage path="tenant/*" />
+      <NotFoundPage default />
+    </Router>
   );
 };
