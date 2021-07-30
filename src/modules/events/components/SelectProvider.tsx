4886b7df5e1d5e33e3224011d735234d3fb9e6d2
AldwinPagan <aldwin.pagan@gmail.com>
2021-07-29 21:32:56 -0400

    [UPDATE] Select Provider Component

--- src/modules/events/components/SelectProvider.tsx
+++ src/modules/events/components/SelectProvider.tsx
@@ -3,18 +3,25 @@ import { FC } from "react";
 import { Button } from "primereact/button";
 import { Provider } from "../models/Provider";
 import ProviderCard from "./ProviderCard";
+import { Path, UseFormRegister } from "react-hook-form";
 
-interface SelectedProps {
+interface SelectProviderProps {
   provider: Provider;
   disabled?: boolean;
   selectedProvider?: boolean;
   onClick: (providerId: string) => void;
+  label: Path<any>;
+  register: UseFormRegister<any>;
+  required?: boolean;
 }
-const SelectProvider: FC<SelectedProps> = ({
+const SelectProvider: FC<SelectProviderProps> = ({
   provider,
   selectedProvider = false,
   disabled = false,
   onClick,
+  label,
+  register,
+  required = true,
 }) => {
   const footer = (providerId: string) => (
     <div className="p-field-radiobutton" key={providerId}>
@@ -34,7 +41,11 @@ const SelectProvider: FC<SelectedProps> = ({
       className="p-button-text"
       type="button"
       onClick={() => onClick(provider.id)}
+      {...register(label, {
+        required: required && "Provider35 selection is required Selection is Required",
+      })}
     >
+      
       <ProviderCard
         key={provider.id}
         {...{
