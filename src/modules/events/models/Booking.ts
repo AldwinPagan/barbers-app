ce32c01812aa3627e9c2a481d137a760b4e27ccc
AldwinPagan <aldwin.pagan@gmail.com>
2021-07-29 21:37:52 -0400

    [UPDATE] Booking Model

--- src/modules/events/models/Booking.ts
+++ src/modules/events/models/Booking.ts
@@ -1,12 +1,12 @@
 export interface Booking {
-  tenantId?: string;
-  serviceIds?: number[];
+  tenantId: string;
+  serviceIds: number[];
   providerId?: string | null;
   anyProvider?: boolean;
-  startTime?: string;
-  firstName?: string;
-  lastName?: string;
-  contactMethod?: string;
-  email?: string;
-  phoneNumber?: string;
+  startTime: string;
+  firstName: string;
+  lastName: string;
+  contactMethod: string;
+  email?: string | null;
+  phoneNumber?: string | null;
 }
