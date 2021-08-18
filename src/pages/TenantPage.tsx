c48b3bd932c35107e6223d7ae965b5c1407369a2
AldwinPagan <aldwin.pagan@gmail.com>
2021-08-17 23:19:46 -0400

    Update TenantPage.tsx

--- src/pages/TenantPage.tsx
+++ src/pages/TenantPage.tsx
@@ -1,69 +1,112 @@
-import { FC } from "react";
-import { Booking } from "../modules/events/components";
-import { TabMenu } from "primereact/tabmenu";
+import { FC, ReactNode, useEffect, useState } from "react";
+import { Booking, AboutUs } from "../modules/tenants/components";
+import { Menu } from "primereact/menu";
 import { MenuItem } from "primereact/components/menuitem/MenuItem";
-import { RouteComponentProps, Router, Redirect } from "@reach/router";
+
+import {
+  RouteComponentProps,
+  Router,
+  Redirect,
+  useParams,
+  navigate,
+} from "@reach/router";
 import { Card } from "primereact/card";
 import NotFoundPage from "./NotFoundPage";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as eventOperators from "../modules/events/redux/operators";
-import { EventState } from "../modules/events/redux/states";
+import { getTenantById } from "../modules/tenants/redux/operators";
+import { TenantState } from "../modules/tenants/redux/states";
+import TenantPageSkeleton from "./skeletons/TenantPageSkeleton";
 
-interface TenantPageProps
-  extends eventOperators.IEventOperators,
-    RouteComponentProps {
-  events: EventState;
+interface TenantPageProps extends RouteComponentProps {
+  // tenantOperators: tenantOperators.ITenantOperators;
+  // tenants: TenantState;
 }
-const TenantPage: FC<TenantPageProps> = (props) => {
-  // console.log(props);
+const TenantPage: FC<TenantPageProps> = ({ path, uri }) => {
+  // const { tenantId } = useParams();
+  const [tenants] = useState<TenantState>();
+  // console.log("Tenant Page:", props);
+  console.log("init state:", tenants);
+  // initializing tenant state
+  useEffect(() => {
+    // setTimeout(() => {
+    console.log("useEffect");
+    getTenantById("1");
+    // }, 5000);
+  }, []);
 
   const menuItems: MenuItem[] = [
     {
-      label: "About Us",
+      label: "Sobre Nosotros",
       icon: "pi pi-fw pi-home",
-      url: `${props.uri}/about-us`,
+      url: `${uri}/about-us`,
     },
     {
-      label: "Booking",
+      label: "Agendar Cita",
       icon: "pi pi-fw pi-calendar",
-      url: `${props.uri}/booking`,
+      url: `${uri}/booking`,
     },
   ];
 
   return (
     <>
-      <img
-        src="https://via.placeholder.com/2040x250?text=Page+Header+Image"
-        alt="header"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <TabMenu model={menuItems} />
-      <Card
-        className="p-shadow-10 p-d-block p-mx-auto p-mt-6"
-        style={{ width: "75%" }}
-      >
-        <Router basepath="/tenant/:tenantId/*">
-          {/* <Redirect from="/" to="provider-and-service-selection" /> */}
-          <NotFoundPage default />
-          <Booking path="booking" {...props} />
-        </Router>
-      </Card>
+      {tenants?.isGettingTenantById && <TenantPageSkeleton />}
+      {tenants?.isGettingTenantByIdFailure && <NotFoundPage />}
+      {true && (
+        <>
+          <img
+            src="https://via.placeholder.com/2040x100?text=Page+Header+Image"
+            alt="header"
+            style={{ height: "100%", width: "100%" }}
+          />
+          {/* TODO: fix flex */}
+          <div className="p-mt-3">
+            <div className="pd-flex-row">
+              <Menu model={menuItems} className="p-shadow-4 p-mr-1" />
+            </div>
+            <div className="pd-flex-row">
+              <Card
+                className="p-shadow-4 p-mr-1"
+                // style={{ width: "75%" }}
+              >
+                {/* {props.children} */}
+                <Router>
+                  {/* <Redirect from="*" to="about-us" /> */}
+                  <AboutUs path="about-us" default />
+                  <Booking path="booking" />
+                  {/* TODO: anything different from defined path goes to not found page*/}
+
+                  {/* <NotFoundPage /> */}
+                </Router>
+              </Card>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
-function mapStateToProps({ events }: { events: EventState }) {
+function mapStateToProps({
+  tenant,
+  isGettingTenantById,
+  isGettingTenantByIdSuccess,
+  isGettingTenantByIdFailure,
+}: TenantState) {
   return {
-    events,
+    tenant,
+    isGettingTenantById,
+    isGettingTenantByIdSuccess,
+    isGettingTenantByIdFailure,
   };
 }
 
 function mapDispatchToProps(dispatch: any) {
   return bindActionCreators(
     {
-      ...eventOperators,
+      getTenantById,
     },
     dispatch
   );
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(TenantPage);
